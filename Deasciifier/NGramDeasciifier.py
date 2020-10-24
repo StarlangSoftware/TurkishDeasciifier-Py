c7138a587908fc105fa4b32bc8dd9dc175bada97
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2020-10-24 11:58:20 +0300

    NGramDeasciifier now checks both previous and next word bigram probabilities.

--- Deasciifier/NGramDeasciifier.py
+++ Deasciifier/NGramDeasciifier.py
@@ -26,6 +26,20 @@ class NGramDeasciifier(SimpleDeasciifier):
         super().__init__(fsm)
         self.__nGram = nGram
 
+    def checkAnalysisAndSetRoot(self, sentence: Sentence, index: int) -> Word:
+        """
+        Checks the morphological analysis of the given word in the given index. If there is no misspelling, it returns
+        the longest root word of the possible analyses.
+        @param sentence Sentence to be analyzed.
+        @param index Index of the word
+        @return If the word is misspelled, null; otherwise the longest root word of the possible analyses.
+        """
+        if index < sentence.wordCount():
+            fsmParses = self.fsm.morphologicalAnalysis(sentence.getWord(index).getName())
+            if fsmParses.size() != 0:
+                return fsmParses.getParseWithLongestRootWord().getWord()
+        return None
+
     def deasciify(self, sentence: Sentence) -> Sentence:
         """
         The deasciify method takes a Sentence as an input. First it creates a String list as candidates,
@@ -48,28 +62,35 @@ class NGramDeasciifier(SimpleDeasciifier):
         """
         previousRoot = None
         result = Sentence()
+        root = self.checkAnalysisAndSetRoot(sentence, 0)
+        nextRoot = self.checkAnalysisAndSetRoot(sentence, 1)
         for i in range(sentence.wordCount()):
             word = sentence.getWord(i)
-            fsmParses = self.fsm.morphologicalAnalysis(word.getName())
-            if fsmParses.size() == 0:
+            if root is None:
                 candidates = self.candidateList(word)
                 bestCandidate = word.getName()
                 bestRoot = word
                 bestProbability = 0
                 for candidate in candidates:
-                    fsmParseList = self.fsm.morphologicalAnalysis(candidate)
-                    root = fsmParseList.getFsmParse(0).getWord()
+                    fsmParses = self.fsm.morphologicalAnalysis(candidate)
+                    root = fsmParses.getParseWithLongestRootWord().getWord()
                     if previousRoot is not None:
-                        probability = self.__nGram.getProbability(previousRoot.getName(), root.getName())
+                        previousProbability = self.__nGram.getProbability(previousRoot.getName(), root.getName())
                     else:
-                        probability = self.__nGram.getProbability(root.getName())
-                    if probability > bestProbability:
+                        previousProbability = 0.0
+                    if nextRoot is not None:
+                        nextProbability = self.__nGram.getProbability(root.getName(), nextRoot.getName())
+                    else:
+                        nextProbability = 0.0
+                    if max(previousProbability, nextProbability) > bestProbability:
                         bestCandidate = candidate
                         bestRoot = root
-                        bestProbability = probability
-                previousRoot = bestRoot
+                        bestProbability = max(previousProbability, nextProbability)
+                root = bestRoot
                 result.addWord(Word(bestCandidate))
             else:
                 result.addWord(word)
-                previousRoot = fsmParses.getParseWithLongestRootWord().getWord()
+            previousRoot = root
+            root = nextRoot
+            nextRoot = self.checkAnalysisAndSetRoot(sentence, i + 2)
         return result
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='NlpToolkit-Deasciifier',
-    version='1.0.10',
+    version='1.0.11',
     packages=['Deasciifier'],
     url='https://github.com/olcaytaner/TurkishDeasciifier-Py',
     license='',
