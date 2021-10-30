72c842ee13d077cdf182659a8fa281a9f8da5645
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2021-10-30 19:55:08 +0300

    Updated setup.py and dictionary.

--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
 from setuptools import setup
 
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
     name='NlpToolkit-Deasciifier',
-    version='1.0.13',
+    version='1.0.14',
     packages=['Deasciifier'],
     url='https://github.com/StarlangSoftware/TurkishDeasciifier-Py',
     license='',
     author='olcaytaner',
     author_email='olcay.yildiz@ozyegin.edu.tr',
     description='Turkish Asciifier/Deasciifier Library',
-    install_requires=['NlpToolkit-MorphologicalAnalysis', 'NlpToolkit-NGram']
+    install_requires=['NlpToolkit-MorphologicalAnalysis', 'NlpToolkit-NGram'],
+    long_description=long_description,
+    long_description_content_type='text/markdown'
 )
